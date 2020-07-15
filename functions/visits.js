d23147a2f4f3ee8e7a4a036ab951d4f6249131ba
Matt McElwee <mmcelwee@dmsi.com>
2020-07-15 12:22:36 -0500

    Updated form to have basic handling

--- functions/visits.js
+++ functions/visits.js
@@ -1,5 +1,7 @@
 const got = require('got');
 
+const BASE_URL = 'https://api.planningcenteronline.com/people/v2';
+
 exports.handler = async function(event, context, callback) {
   if (event.method !== 'POST') {
     return callback('Method not supported');
@@ -12,11 +14,41 @@ exports.handler = async function(event, context, callback) {
     phone
   } = JSON.parse(event.body);
 
+  const personData = {
+    data: {
+      type: 'person',
+      attributes: {
+        first_name: firstName,
+        last_name: lastName,
+        primary_campus_id: '48766'
+      },
+    },
+  };
+
+  const emailData = {
+    address: email,
+    location: 'home',
+    primary: true,
+  };
+
+  const phoneData = phone && {
+    number: phone,
+    location: 'home',
+    primary: true,
+  };
+
+  const [person, err] = await got(`${BASE_URL}/people`, {
+    method: 'POST',
+    body: JSON.stringify(personData),
+    username: process.env.PCO_USERNAME,
+    password: process.env.PCO_PASSWORD,
+  }).json();
+
   return callback(null, {
     statusCode: 200,
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
-
+      person
     }),
   });
 };
